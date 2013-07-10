979bedba91c1dee043dc8e26b85e7b658f2fd1f1
gordanaskoric <gordanaskoric@yahoo.com>
2013-07-10 23:42:35 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,14 +1,13 @@
-
-var express = reqire('express');
-var fs = require ('fs');
+var express = require('express');
 var app = express.createServer(express.logger());
-var content = fs.readFileSync('index.html','utf-8');
 
 app.get('/', function(request, response) {
-     response.send(content);
-});
+var fs = require('fs');
+var buffer = new buffer ();
+  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
